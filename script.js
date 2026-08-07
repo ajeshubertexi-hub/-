09ee6f454802997e539e57a79ebb793fb23ae88e
ajeshubertexi-hub <ajeshubertexi@gmail.com>
2026-08-08 01:45:20 +0530

    Update script.js
    "Updated JavaScript functionality for online token booking, generating customer token numbers and displaying token slip details for Aadhar Seva Kendra Kanjouli."

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let tokenNumber = 1;
 
 function bookToken() {
-    let name = document.getElementById("name").value;
-    let service = document.getElementById("service").value;
-    
-    if(name === "") {
-        alert("कृपया अपना नाम लिखें");
-        return;
-    }
 
-    let token = "A-" + String(tokenNumber).padStart(3, '0');
+let name = document.getElementById("name").value;
+let service = document.getElementById("service").value;
 
-    alert(
-        "टोकन बुक हो गया!\n\n" +
-        "नाम: " + name +
-        "\nसेवा: " + service +
-        "\nटोकन नंबर: " + token
-    );
+if(name === ""){
+alert("कृपया अपना नाम लिखें");
+return;
+}
 
-    tokenNumber++;
+let token = "A-" + String(tokenNumber).padStart(3,'0');
+
+document.getElementById("tokenSlip").style.display = "block";
+
+document.getElementById("tokenNumber").innerHTML =
+"टोकन नंबर: " + token;
+
+document.getElementById("customerName").innerHTML =
+"नाम: " + name;
+
+document.getElementById("customerService").innerHTML =
+"सेवा: " + service;
+
+tokenNumber++;
+
 }
